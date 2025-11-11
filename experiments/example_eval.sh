375b51f89debb4d79b510417eda88c9619b6372e
NWUzhouwei <87263640+NWUzhouwei@users.noreply.github.com>
2025-11-11 14:43:44 +0800

    Update example_eval.sh

--- experiments/example_eval.sh
+++ experiments/example_eval.sh
@@ -1,7 +1,7 @@
 cd evaluation
 
-export pred_dir="/home/sxz/下载/BDM-main/experiments/outputs/train_sofa_samplebing_pc_1_pix/2025-03-06--17-07-24/sample/pred/sofa/" # (e.g., ".../sample/pred/chair")
-export gt_dir="/home/sxz/下载/BDM-main/experiments/outputs/train_sofa_samplebing_pc_1_pix/2025-03-06--17-07-24/sample/gt/sofa/" # (e.g., ".../sample/gt/chair")
+export pred_dir="/home/pdm/download/BDM-main/experiments/outputs/train_sofa_samplebing_pc_1_pix/2025-03-06--17-07-24/sample/pred/sofa/" # (e.g., ".../sample/pred/chair")
+export gt_dir="/home/pdm/download/BDM-main/experiments/outputs/train_sofa_samplebing_pc_1_pix/2025-03-06--17-07-24/sample/gt/sofa/" # (e.g., ".../sample/gt/chair")
 
 echo "----------"
 echo "sample_chair_pc2_r2n2_0.1"
