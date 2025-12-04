c6772a0ade42068afb7c14ff01555aac84c45c8d
NWUzhouwei <87263640+NWUzhouwei@users.noreply.github.com>
2025-12-04 09:35:46 +0800

    Update example_sample_fusion.sh

--- experiments/example_sample_fusion.sh
+++ experiments/example_sample_fusion.sh
@@ -17,7 +17,7 @@ export roll_step=16
 # milestones for interaction
 export milestones="[1000,968,936,872,128,64,32,0]"
 
-python main_blending.py \
+python main_ge.py \
     logging.wandb_project=${wandb_name} \
     run.job=sample_bdm_blending \
     run.save_dir=${save_dir} \
